7be0d44526346cdb1e591cf999518643c07a9497
CharlesTitzer <75635294+CharlesTitzer@users.noreply.github.com>
2024-05-04 11:34:41 -0500

    Carl's third person character controller

---
+++ Assets/Scripts/enableComponents.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Mirror;
+using Cinemachine;
+
+namespace StarterAssets
+{
+
+
+    public class EnableComponents : NetworkBehaviour
+    {
+        public Transform target;
+        // Start is called before the first frame update
+        void Start()
+        {
+            if (isLocalPlayer)
+            {
+                CharacterController cc = GetComponent<CharacterController>();
+                cc.enabled = true;
+                ThirdPersonController tpc = GetComponent<ThirdPersonController>();
+                tpc.enabled = true;
+                PlayerInput pi = GetComponent<PlayerInput>();
+                pi.enabled = true;
+                GameObject pfc = GameObject.Find("PlayerFollowCamera");
+                CinemachineVirtualCamera cvc = pfc.GetComponent<CinemachineVirtualCamera>();
+                cvc.Follow = target;
+            }
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
